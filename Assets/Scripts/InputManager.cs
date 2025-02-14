abfea70713384f0dca2b740d1c78b4ebde01116f
sol9342 <solanag44@gmail.com>
2025-02-14 14:02:57 -0800

    Forward motion (work in progress)

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,15 +6,18 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnWPressed = new UnityEvent();
 
     void Update()
     {
         Vector3 input = Vector3.zero;
 
+        
         if(Input.GetKey(KeyCode.W))
         {
             input += Vector3.up;
         }
+        
         if(Input.GetKey(KeyCode.A))
         {
             input += Vector3.left;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,25 @@ public class Player : MonoBehaviour
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float speed;
     private Rigidbody rb;
+    [SerializeField] private Transform forwardMovement;
 
     void Start()
     {
         inputManager.OnMove.AddListener(MovePlayer);
         rb = GetComponent<Rigidbody>();
+        inputManager.OnWPressed.AddListener(MoveForward);
     }
 
     private void MovePlayer(Vector2 direction)
     {
         Vector3 moveDirection = new(direction.x, 0f, direction.y);
-        rb.AddForce(speed * moveDirection);
+        //rb.AddForce(speed * moveDirection);
+        //rb.AddForce(forwardMovement.forward * speed);
+    }
+
+    private void MoveForward()
+    {
+        rb.AddForce(forwardMovement.forward * speed);
     }
 
 
