92f7ab50d82eb9eff68234d9e4338356ea623751
sol9342 <solanag44@gmail.com>
2025-02-23 20:06:05 -0800

    Added player jump

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,11 +7,20 @@ using Unity.Cinemachine;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent<Vector3> OnJump = new UnityEvent<Vector3>();
     [SerializeField] private Transform forwardMovement;
+    //private bool isTouching;
+/*
+    private void OnCollisionStay()
+    {
+        isTouching = true;
+    }
+    */
 
     void Update()
     {
         Vector3 input = Vector3.zero;
+        Vector3 jump = Vector3.zero;
         
         if(Input.GetKey(KeyCode.W))
         {
@@ -25,8 +34,14 @@ public class InputManager : MonoBehaviour
         {
             input += forwardMovement.right;
         } 
+        if(Input.GetKey(KeyCode.Space))
+        {
+            jump += Vector3.up;
+            //OnJump?.Invoke(jump);
+            //isTouching = false;
+        }
         
-
         OnMove?.Invoke(input);
+        OnJump?.Invoke(jump);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,53 @@ using Unity.Cinemachine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
     private Rigidbody rb;
+    private bool isTouching;
 
     void Start()
     {
         inputManager.OnMove.AddListener(MovePlayer);
+        inputManager.OnJump.AddListener(JumpPlayer);
         rb = GetComponent<Rigidbody>();
     }
+/*
+    private void OnCollisionStay()
+    {
+        isTouching = true;
+    }
+    */
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isTouching = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isTouching = false;
+        }
+    }
+
 
     public void MovePlayer(Vector3 direction)
     {
         Vector3 moveDirection = new(direction.x, 0f, direction.z);
-        rb.AddForce(speed * moveDirection);
+        rb.AddForce(moveSpeed * moveDirection);
+    }
+
+    public void JumpPlayer(Vector3 dir)
+    {
+        if(isTouching)
+        {
+            rb.AddForce(dir * jumpSpeed, ForceMode.Impulse);
+            //isTouching = false;
+        }
     }
 
 }
